17e119068ae8a7237d1ce3cbc8a9e2151a27df33
Ana Beatriz Bento Broetto <alphawolf244@gmail.com>
2026-05-05 08:39:24 -0400

    colocando video na home do projeto, estilizando o projeto
    colocando video na home do projeto, estilizando o projeto

--- organo/src/componentes/Banner/index.js
+++ organo/src/componentes/Banner/index.js
@@ -1,11 +1,16 @@
 // IMPORTANDO O ARQUIVO CSS PARA ESTILIZAR O BANNER
+import React, {Component} from "react";
 import "./Banner.css";
 
 const Banner = () => {
   return (
     <header className="banner">
-      <img src="./imagens/banner.png" alt="o banner principal da pagina do organo!"/>
+      <video className="back-video" autoPlay loop muted playsInline controls>
+      <source src="/organo/public/videos/fundo-home.mp4"  type="video/mp4"/></video>
+      <div className="titulo-home">
+        <h1>PeopleHub</h1>
+      </div>
     </header>
   );
-}
+};
 export default Banner; // exporta a função para ser usada em outros lugares, arquivos.
