f40c44665193d975709edbe5c6abbd35c1ed6387
Ana Beatriz Bento Broetto <alphawolf244@gmail.com>
2026-05-06 09:55:19 -0400

    estilizando o projeto e deixando responsivo, section home
    estilizando o projeto e deixando responsivo, section home

--- organo/src/componentes/Banner/index.js
+++ organo/src/componentes/Banner/index.js
@@ -1,14 +1,20 @@
 // IMPORTANDO O ARQUIVO CSS PARA ESTILIZAR O BANNER
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./Banner.css";
+import myVideo from "../../videos/fundo-home.mp4";
 
 const Banner = () => {
   return (
     <header className="banner">
-      <video className="back-video" autoPlay loop muted playsInline controls>
-      <source src="/organo/public/videos/fundo-home.mp4"  type="video/mp4"/></video>
-      <div className="titulo-home">
-        <h1>PeopleHub</h1>
+      <video className="back-video" src={myVideo} autoPlay muted loop></video>
+      <div className="container-principal">
+        <div className="titulo-home">
+          <h1>PeopleHub</h1>
+          <p>Pessoas e equipes, organizados em um só lugar!</p>
+        </div>
+        <div className="image-home">
+          <img src="./imagens/img-equipe-home.png" alt="" />
+        </div>
       </div>
     </header>
   );
